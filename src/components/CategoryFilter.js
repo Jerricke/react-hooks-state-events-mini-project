aed1e07dfbdb47404ae9955af509c8db85da39da
Jerricke <eejerrickw@gmail.com>
2023-07-05 14:24:04 -0600

    complete

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, handleClick}) {
+
+
+  function CategoryBtn({context}) {
+    return (
+      <button 
+      name={context}
+      onClick={handleClick}
+      >{context}</button>
+    )
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map( (cate, index) => (
+        <CategoryBtn key={index} context={cate}/>
+      ))}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, selected}) {
+
+  const taskToDisplay = tasks.filter( (task) => {
+    if (selected === "All") return true
+    else return selected === task.category;
+  })
+
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {taskToDisplay.map( (item, index) => {
+        return <Task key={index} category={item.category} text={item.text} />
+      })}
     </div>
   );
 }
